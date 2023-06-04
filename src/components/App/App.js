6ab466ab267ab2db55d30b5199e56ac4de2ce0ba
shaeeb <shaeebali@gmail.com>
2023-06-05 00:40:30 +0100

    add css/configure app.js/add favicons/backgroud images

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,18 @@ import React from "react";
 import ReactDOM from 'react-dom';
 
 function App() {
-  return <h1>Hello world!</h1>
+  return (
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div class="App">
+          {/* Add a SearchBar component here */}
+        <div className="App-playlist">
+          {/* Add a SearchResults component here */}
+          {/* Add a Playlist component here */}
+        </div>
+        </div>
+    </div>
+  );
 }
 
 export default App;
