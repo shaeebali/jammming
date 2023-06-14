cacd78e155e31d1f7f5d045b3257c522b973e698
shaeeb <shaeebali@gmail.com>
2023-06-14 11:19:10 +0100

    refer to spotify dev docs and code out relevant async functions

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,9 +1,21 @@
-const clientId = ''
-
+const clientId = 'bd8aba796b9249329067c9e5dd1b7e27';
+const code = undefined;
 const accessToken = '';
 const spotify = {};
 
-function getAccessToken() {
+if (!code) {
+  redirectToAuthCodeFlow(clientId);
+} else {
+  const accessToken = await getAccessTokenFromCodeFlow(clientId, code);
+  const profile = await fetchProfile(accessToken);
+  populateUI(profile);
+}
+
+async function redirectToAuthCodeFlow(clientId) {
+
+}
+
+async function getAccessToken(clientId, code) {
 
 }
 
@@ -13,6 +25,10 @@ async function fetchProfile(token) {
   });
 
   return await result.json();
+}
+
+async function fetchProfile(profile) {
+
 }
 
 export {spotify};
