a7f019af937a1b07db7a940105ef79f3a3867020
shaeeb <shaeebali@gmail.com>
2023-06-15 15:41:49 +0100

    work on save playlist feature

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,26 +5,34 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
-import { spotifySearch } from '../../util/Spotify';
+import PlaylistList from '../PlaylistList/PlaylistList';
+import { spotifySearch, savePlaylist, getUserPlaylists, getPlaylistTracks } from '../../util/Spotify';
 
 function App() {
   //add state variables below:
   // const [results, setResults] = useState([]);
   
   const [searchResults, setSearchResults] = useState([])
-
   const [searchTerm, setSearchTerm] = useState('');
-
   const [playlistTitle, setPlaylistTitle] = useState([]);
-
-  const [playlistName, setPlaylistName] = useState([]);
-  
+  const [playlistName, setPlaylistName] = useState(['My Playlist']);
   const [playlistTracks, setPlaylistTracks] = useState([]);
-
   const [playlists, setPlaylists] = useState([]);
 
   //add additional functions below:
 
+  async function onSelectPlaylist(id, name) {
+    const tracks = await getPlaylistTracks(id)
+    console.log("tracks= ", tracks);
+    setPlaylistTracks(tracks);
+    setPlaylistName(name);
+  }
+  
+  async function onGetPlaylists(){
+    const lists = await getUserPlaylists();
+    setPlaylists(lists);
+  }
+  
   function onAdd(track) {
     
     if (!playlistTracks.find(playlistTrack => playlistTrack.id === track.id)) {
@@ -41,7 +49,10 @@ function App() {
   }
 
   function onSave() {
-    const trackURIs = playlistTracks.map(playlistTrack => playlistTrack.uri);
+    let savedList = playlistTracks.map(playlistTrack => playlistTrack.uri);
+    savePlaylist(playlistName, savedList);
+    setSearchResults([]);
+    setPlaylistName('My Playlist');
   }
 
   function onSearchTerm(term) {
